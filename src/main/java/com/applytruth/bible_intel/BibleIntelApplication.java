c4346829a33a2c9bd9e43dba9a80dd7a0f9af1e9
servantfist <applytruth@gmail.com>
2024-09-23 12:12:08 -0600

    Initial Commit

---
+++ src/main/java/com/applytruth/bible_intel/BibleIntelApplication.java
@@ -0,0 +1,13 @@
+package com.applytruth.bible_intel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleIntelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleIntelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applytruth/bible_intel/BibleIntelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applytruth.bible_intel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleIntelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
